27afaac2d21f530aa757f9e87da321294e149e74
Ghevond Tunyan <ghtunyan2018@gmail.com>
2022-12-07 19:55:45 +0100

    according to  test driven development approach added tests for inputs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  return <div></div>
+  return (
+    <div className='container my-5'>
+      <form>
+        <div className='mb-3'>
+          <label htmlFor='email' className='form-label'>
+            Email address
+          </label>
+          <input
+            type='email'
+            id='email'
+            className='form-control'
+            name='email'
+          />
+        </div>
+        <div className='mb-3'>
+          <label htmlFor='password' className='form-label'>
+            Password
+          </label>
+          <input
+            type='password'
+            id='password'
+            className='form-control'
+            name='password'
+          />
+        </div>
+        <div className='mb-3'>
+          <label htmlFor='confirm-password' className='form-label'>
+            Confirm Password
+          </label>
+          <input
+            type='password'
+            id='confirm-password'
+            className='form-control'
+            name='confirm-password'
+          />
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,53 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import App from './App'
 
 // 1) Rendering the component we want to test
-// 2) Fining the elements
+// 2) Finding the elements
 // 3)Assertion
+// test driven development approach
+
+test('inputs should be initially empty', () => {
+  render(<App />)
+  const emailInputElement = screen.getByRole('textbox')
+  const passwordInputElement = screen.getByLabelText('Password')
+  const confirmPasswordInputElement = screen.getByLabelText(/confirm password/i)
+  expect(emailInputElement.value).toBe('')
+  expect(passwordInputElement.value).toBe('')
+  expect(confirmPasswordInputElement.value).toBe('')
+})
+
+test('should be able to type an email', () => {
+  render(<App />)
+  const emailInputElement = screen.getByRole('textbox', {
+    name: /email/i,
+  })
+  userEvent.type(emailInputElement, 'selena@gmail.com')
+  expect(emailInputElement.value).toBe('selena@gmail.com')
+})
+test('should be able to type an password', () => {
+  render(<App />)
+  const emailInputElement = screen.getByLabelText('Password')
+  userEvent.type(emailInputElement, 'password!')
+  expect(emailInputElement.value).toBe('password!')
+})
+
+test('should be able to type an confirm password', () => {
+  render(<App />)
+  const confirmEmailPassword = screen.getByLabelText(/confirm password/i)
+  userEvent.type(confirmEmailPassword, 'password!')
+  expect(confirmEmailPassword.value).toBe('password!')
+})
+
+test('should show email error massage on invalid email', () => {
+  const emailInputElement = screen.getByRole('textbox', {
+    name: /email/i,
+  })
+  userEvent.type(emailInputElement, 'selenagmail.com')
+  const submitBtnElement = screen.getByRole('button', {
+    name: /submit/i,
+  })
+  userEvent.click(submitBtnElement)
+  const emailErrorElement = screen.getByText(/the email you input is invalid/i)
+  expect(emailErrorElement).toBeInTheDocument
+})
